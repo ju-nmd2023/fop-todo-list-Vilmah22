ff6bde9e8fb27fc2fac93d60cf5c4a01ab972add
Vilmah22 <havi22tu@student.ju.se>
2024-04-14 14:33:50 +0200

    local storage

--- Todo-list-js/todo.js
+++ Todo-list-js/todo.js
@@ -4,18 +4,33 @@ const inputElement = document.getElementById("writeATask");
 const addUlItem = document.getElementById("list-container");
 
 window.addEventListener("load", () => {
+  const tasksInStorage = localStorage.getItem("tasks");
+  if (tasksInStorage) {
+    tasks = JSON.parse(tasksInStorage);
+    showTasks();
+  }
+
   const addButtonElement = document.querySelector(".taskConfirm");
   addButtonElement.addEventListener("click", addTask);
 });
 
 function addTask() {
   let inputFieldText = inputElement.value;
+
+  // this line of code was retrieved from my dad
+  if (inputFieldText == "") {
+    return;
+  }
+  //
+
   let task = {
     taskName: inputFieldText,
-    done: "images/checked.jpeg",
-    remove: "images/remove.jpeg",
+    statusIsChecked: false,
   };
   tasks.push(task);
+  // this line of code was retrieved from: https://blog.logrocket.com/localstorage-javascript-complete-guide/ Accessed: 14/04/24
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  //
   showTasks();
   inputElement.value = "";
 }
@@ -23,6 +38,7 @@ function addTask() {
 function showTasks() {
   addUlItem.innerHTML = "";
   for (i = 0; i < tasks.length; i++) {
+    let task = tasks[i];
     let liTaskElement = document.createElement("li");
 
     liTaskElement.innerText = tasks[i].taskName;
@@ -30,19 +46,21 @@ function showTasks() {
     addUlItem.appendChild(liTaskElement);
 
     const imgElement = document.createElement("img");
-    imgElement.classList.add(".imagechecked");
-    imgElement.src = tasks[i].done;
+    imgElement.classList.add("imagechecked");
+    imgElement.src = "images/checked.jpeg";
     imgElement.height = 40;
     addUlItem.appendChild(imgElement);
 
     imgElement.addEventListener("click", function () {
       liTaskElement.style.textDecoration = "line-through";
+      task.statusIsChecked = true;
+      localStorage.setItem("tasks", JSON.stringify(tasks));
     });
     addUlItem.appendChild(imgElement);
 
     const imgElement2 = document.createElement("img");
-    imgElement2.classList.add(".imageremove");
-    imgElement2.src = tasks[i].remove;
+    imgElement2.classList.add("imageremoved");
+    imgElement2.src = "images/remove.jpeg";
     imgElement2.height = 40;
     addUlItem.appendChild(imgElement2);
 
@@ -54,9 +72,3 @@ function showTasks() {
     });
   }
 }
-
-// function add {
-//   itemsArray.push(input.value)
-
-// }
-// localStorage
